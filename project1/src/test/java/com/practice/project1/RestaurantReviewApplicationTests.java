7d35a5d8d994f55604cd501502f51efc30b34e97
johnmramirez <johnmichaelramirez7@gmail.com>
2023-11-24 21:29:13 -0600

    Added build files

---
+++ project1/src/test/java/com/practice/project1/RestaurantReviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.project1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantReviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
