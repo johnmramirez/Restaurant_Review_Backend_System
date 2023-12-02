feb256fd9d71bad635f5380dbd53afdabe116970
johnmramirez <johnmichaelramirez7@gmail.com>
2023-12-02 15:32:00 -0600

    swapped Jakarta Id import for spring data id import, added oauth2 spring starter dependency

--- project2/src/main/java/com/practice/project2/Review.java
+++ project2/src/main/java/com/practice/project2/Review.java
@@ -1,7 +1,7 @@
 package com.practice.project2;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import jakarta.persistence.Id;
+import org.springframework.data.annotation.Id;
 
 import java.util.Date;
 
